c8c9e91976fb0d00b0830e293e04596cad3d113c
mdreaz9624 <mreaz9624@gmail.com>
2026-05-08 08:57:16 +0600

    fixed vercel problems

--- index.js
+++ index.js
@@ -48,16 +48,6 @@ async function run() {
       const result = await universityDataCollection.insertOne(studyData);
       res.send(result);
     });
-
-
-
-
-
-
-
-
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
